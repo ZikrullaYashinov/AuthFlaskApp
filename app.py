849668093b00d8c0284d97924b20069fd0745137
zikrulla <zikrullayashinov173@gmail.com>
2024-03-02 19:44:16 +0500

    edit 4

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from auth.views import auth_blueprint
+# from auth.views import auth_blueprint
 from api.views import blueprint
 from config import FLASK_RUN_HOST, FLASK_RUN_PORT
 from extensions import db, migrate, jwt
 
 app = Flask(__name__)
 app.register_blueprint(blueprint=blueprint)
-app.register_blueprint(blueprint=auth_blueprint)
+# app.register_blueprint(blueprint=auth_blueprint)
 app.config.from_object('config')
 
 db.init_app(app)
